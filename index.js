0d7e299ce92ecbdee3c54802c64c5de6d736c1a8
swastik-akhil <akhilmindset@gmail.com>
2023-10-02 00:08:43 +0530

    Added views and frontend

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const path = require('path');
+const URL = require('./models/url');
 const {PORT} = process.env;
 //db connection
 require('./dbConnection').dbConnection();
+
+//ejs
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("./views"))
 
 //middlewares
 app.use(express.json());
@@ -12,8 +18,14 @@ app.use(express.urlencoded({extended : true}));
 //routers
 const urlRouter = require('./routes/url')
 app.use('/api/v1/url', urlRouter);
-app.use('/api/v1/:shortUrl', urlRouter);
-
+app.use('/api/v1/url/:shortUrl', urlRouter);
+app.get('/test', async (req,res)=>{
+	const allUrl = await URL.find({})
+	return res.render('index', {
+		allUrl : allUrl,
+		PORT : PORT
+	})
+})
 
 
 
